cf8c27390bb207019e698a5cfd968fbc25e4b75d
Ilya Skryabin <808lorem@gmail.com>
2017-05-06 20:22:29 +0300

    Добавил функцию изменения href="#id" кнопки "вниз" в зависимости от id соседней секции

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -8,20 +8,50 @@ $(document).ready(function(){
 	
 	
 	// Определяю оптимальную высоту top-banner
-	var headerPaddingTop = parseFloat($('.header').css('padding-top')),
-		headerPaddingBottom = parseFloat($('.header').css('padding-bottom')),
-		headerMarginTop = parseFloat($('.header').css('margin-top')),
-		headerMarginBottom = parseFloat($('.header').css('margin-bottom'));
+	var headerPaddingTop = parseFloat($('#header').css('padding-top')),
+		headerPaddingBottom = parseFloat($('#header').css('padding-bottom')),
+		headerMarginTop = parseFloat($('#header').css('margin-top')),
+		headerMarginBottom = parseFloat($('#header').css('margin-bottom'));
 	
-	var headerHeightSum = $('.header').height() + headerPaddingTop + headerPaddingBottom + headerMarginTop + headerMarginBottom;
+	var headerHeightSum = $('#header').height() + headerPaddingTop + headerPaddingBottom + headerMarginTop + headerMarginBottom;
 	
 		// установим обработчик события resize
 	$(window).resize(function(){
 		var h_w = $(window).height();
 		var topBannerHeight = h_w - headerHeightSum;
-		$('.top-banner').css('height', topBannerHeight);
+		$('#top-banner').css('height', topBannerHeight);
 	});
 
 	// вызовем событие resize
 	$(window).resize();
+	
+	
+	// Скрол
+	$('.scroll-link').click(function() {
+	  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		if (target.length) {
+		$('html, body').animate({
+		  scrollTop: target.offset().top
+		}, 1000);
+		return false;
+		}
+	  }
+	});
+	
+	
+	// Изменить href='#id' кнопки вниз
+	// в зависимости от #id соседней секции
+	function idNextSection () {
+		var getvalue = $('#top-banner__arrow_link').attr('href'),
+			nextAtteId = '#' + $('#top-banner').next().attr('id');
+		
+		if(getvalue !== nextAtteId) {
+			$('#top-banner__arrow_link').attr('href', nextAtteId)
+		}
+	};
+	
+	idNextSection();
+
 });
