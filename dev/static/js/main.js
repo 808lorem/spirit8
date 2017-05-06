576d0fb69446480d504c6c1f6cdf30af57cbd213
Ilya Skryabin <808lorem@gmail.com>
2017-05-06 17:22:01 +0300

    Добавил обработчик событий для секции top-banner, чтобы при изменении экрана изменялась высота. top-banner = window - header

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -5,4 +5,23 @@ $(document).ready(function(){
 		$('.active').removeClass('active');
 		$(this).addClass('active');
 	});
+	
+	
+	// Определяю оптимальную высоту top-banner
+	var headerPaddingTop = parseFloat($('.header').css('padding-top')),
+		headerPaddingBottom = parseFloat($('.header').css('padding-bottom')),
+		headerMarginTop = parseFloat($('.header').css('margin-top')),
+		headerMarginBottom = parseFloat($('.header').css('margin-bottom'));
+	
+	var headerHeightSum = $('.header').height() + headerPaddingTop + headerPaddingBottom + headerMarginTop + headerMarginBottom;
+	
+		// установим обработчик события resize
+	$(window).resize(function(){
+		var h_w = $(window).height();
+		var topBannerHeight = h_w - headerHeightSum;
+		$('.top-banner').css('height', topBannerHeight);
+	});
+
+	// вызовем событие resize
+	$(window).resize();
 });
