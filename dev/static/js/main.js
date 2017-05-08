fa4272ae712cd72c5dae615697ed28061a495688
Ilya Skryabin <808lorem@gmail.com>
2017-05-08 19:07:22 +0300

    Скрипты для слайдеров и фильтра по типу

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -49,4 +49,65 @@ $(document).ready(function(){
 		return false;
 	});
 
+	
+	$('.js-slider__team').slick({
+		dots: true,
+		arrows: false,
+		speed: 900,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+//		responsive: [
+//		{
+//		  breakpoint: 1024,
+//		  settings: {
+//			slidesToShow: 3,
+//			slidesToScroll: 3,
+//			infinite: true,
+//			dots: true
+//		  }
+//		}
+//		]
+	});
+	
+	$('.js-slider__clients').slick({
+		dots: true,
+		arrows: false,
+		speed: 900,
+		slidesToShow: 5,
+		slidesToScroll: 5,
+//		responsive: [
+//		{
+//		  breakpoint: 1024,
+//		  settings: {
+//			slidesToShow: 3,
+//			slidesToScroll: 3,
+//			infinite: true,
+//			dots: true
+//		  }
+//		}
+//		]
+	});
+	
+	
+	$(function() {
+
+		var newSelection = "";
+
+		$("#flavor-nav a").click(function(){
+
+			$("#all-flavors").fadeTo(200, 0.10);
+
+			$("#flavor-nav a").removeClass("current");
+			$(this).addClass("current");
+
+			newSelection = $(this).attr("rel");
+
+			$(".flavor").not("."+newSelection).slideUp();
+			$("."+newSelection).slideDown();
+
+			$("#all-flavors").fadeTo(600, 1);
+
+		});
+
+	});
 });
